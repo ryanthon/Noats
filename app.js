7832582f2ee85d05e0ebd48f34ac559a8d7432a8
Ryan Khalili <rykhalil4@gmail.com>
2015-02-16 01:02:12 -0800

    Added new dependencies. Updated express and handlebars.

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
  */
 
 var express = require('express');
-var http = require('http');
 var path = require('path');
-var handlebars = require('express3-handlebars')
+var handlebars = require('express-handlebars')
+var bodyparser = require('body-parser');
 
 var index = require('./routes/index');
 var signIn = require('./routes/sign-in');
@@ -15,28 +15,18 @@ var mynotes = require('./routes/mynotes');
 var uploadNotes = require('./routes/upload_notes');
 var classes = require('./routes/classes');
 
-
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 8000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', handlebars());
+app.engine('handlebars', handlebars() );
 app.set('view engine', 'handlebars');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
-app.use(express.cookieParser('Intro HCI secret key'));
-app.use(express.session());
-app.use(app.router);
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 // Add routes here
 app.get('/', index.view);
@@ -49,6 +39,6 @@ app.post('/upload/notes', uploadNotes.upload);
 app.get('/classes', classes.singleClassView);
 app.get('/classes/subjects', classes.viewSubjects);
 
-http.createServer(app).listen(app.get('port'), function(){
+app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
